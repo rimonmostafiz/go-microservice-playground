df5ba39404d94e1ae41395c0d1fc527bbb198911
Rimon Mostafiz <rimonmostafiz@gmail.com>
2020-11-15 20:06:13 +0600

    Implement add product to existing product list by POST request

--- src/data/products.go
+++ src/data/products.go
@@ -21,6 +21,11 @@ type Product struct {
 
 type Products []*Product
 
+func (p *Product) FromJSON(r io.Reader) error {
+	d := json.NewDecoder(r);
+	return d.Decode(p)
+}
+
 func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(p)
@@ -28,6 +33,16 @@ func (p *Products) ToJSON(w io.Writer) error {
 
 func GetProducts() Products {
 	return productList
+}
+
+func AddProduct(p *Product)  {
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
+func getNextID() int {
+	lastProduct := productList[len(productList) - 1]
+	return lastProduct.ID + 1
 }
 
 var productList = []*Product{
--- src/handlers/products.go
+++ src/handlers/products.go
@@ -15,10 +15,38 @@ func NewProducts(logger *log.Logger) *Products {
 }
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request)  {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		p.addProducts(rw, r)
+		return
+	}
+
+	// catch all
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.logger.Println("Handle GET Product")
 	listOfProducts := data.GetProducts()
 
 	err := listOfProducts.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProducts(rw http.ResponseWriter, r *http.Request) {
+	p.logger.Println("Handle POST Product")
+
+	product := &data.Product{}
+	err := product.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+	p.logger.Printf("Prod: %#v", product)
+	data.AddProduct(product)
 }
